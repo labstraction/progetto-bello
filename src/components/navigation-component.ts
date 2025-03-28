278a5994fb2fca6f1e27452e4d706c9473e3c82b
janstigliani <janstigli@gmail.com>
2025-03-28 13:30:53 +0100

    Add navigation component styles and update structure

--- src/components/navigation-component.ts
+++ src/components/navigation-component.ts
@@ -21,6 +21,44 @@ export default class NavigationComponent extends HTMLElement{
         const style = document.createElement('style');
         style.innerText = `
 
+        img{
+            height: 24px;
+        }
+
+        .nav-container{
+            display: flex;
+            min-height: 80px;
+            width: 100%;
+            justify-content: space-between;
+        }
+
+        .home-link{
+            width: 88px;
+        }
+
+        .home-logo{
+            width:88px;
+            height: 88px;
+            border-radius: 24px;
+        }
+
+        .order-button{
+            background-color: transparent;
+            border: none;
+        }
+
+        .add-button{
+            border-radius: 50%;
+            font-size:1.4rem;
+            font-weight:900;
+            background-color: transparent;
+        }
+
+        .add-button:hover{
+            font-size:1.7rem;
+        }
+
+        
         `
         this.shadowRoot!.appendChild(style);
     }
@@ -33,11 +71,41 @@ export default class NavigationComponent extends HTMLElement{
         } else {
             mainDiv = document.createElement('div');
             mainDiv.id = 'navigation-container';
+            mainDiv.classList.add("nav-container")
         }
 
-        mainDiv.innerHTML = 'sono la barra di navigazione'
+        mainDiv.innerHTML = `
+        <a class="home-link" href="./index.html">
+            <img class="home-logo" src="../public/logo-placeholder.jpg" alt="">
+        </a>
+        <h1>Too Good Not To Do</h1>
+        `;
+
+        const orderBtn = document.createElement("button");
+        const orderIcon = document.createElement("img");
+        orderBtn.classList.add("order-button");
+        orderIcon.classList.add("order-img");
+        orderIcon.src = "https://cdn-icons-png.freepik.com/256/13086/13086816.png?semt=ais_hybrid";
+        orderBtn.appendChild(orderIcon);
+        orderBtn.addEventListener("click", () => this.dispatchChangeOrder());
+        mainDiv.appendChild(orderBtn);
+
+        const link = document.createElement("a");
+        link.href="./index/#/new"
+        const addBtn = document.createElement("button");
+        addBtn.classList.add("add-button")
+        const addNode = document.createTextNode("+");
+        addBtn.appendChild(addNode);
+        link.appendChild(addBtn);
+        mainDiv.appendChild(link);
 
         this.shadowRoot!.appendChild(mainDiv);
+    }
+
+    dispatchChangeOrder(): any {
+        const event = new CustomEvent(`change-order`);
+        console.log(event);
+        document.dispatchEvent(event);
     }
 
 
